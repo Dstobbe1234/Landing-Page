4b97658e2f732deba6ab071dde3792e5d9a1fcbd
David Stobbe <d.stobbe1@share.epsb.ca>
2022-02-08 13:27:45 -0700

    2/8/2022

---
+++ script.js
@@ -0,0 +1,8 @@
+// Landing Page by David S.
+
+// Prompt user for data
+let name = prompt("What is your name?");
+let focus = promt("What is your main focus for today?");
+
+// Output Data into the page
+document.getElementById("nameSpan").innerHTML = name;
