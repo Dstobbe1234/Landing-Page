f513ccab378cbab4546d3051510ba21062ea8e4c
David Stobbe <d.stobbe1@share.epsb.ca>
2022-02-09 12:35:14 -0700

    Done

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Landing Page by David S.
 
 // Prompt user for data
-let name = prompt("What is your name?");
-let focus = promt("What is your main focus for today?");
+let name_ = prompt("What is your name?");
+let focus_ = prompt("What is your main focus for today?");
 
 // Output Data into the page
-document.getElementById("nameSpan").innerHTML = name;
-document.getElementById("focus!").innerHTML = focus;
+document.getElementById("nameSpan").innerHTML = name_;
+document.getElementById("focus").innerHTML = focus_;
